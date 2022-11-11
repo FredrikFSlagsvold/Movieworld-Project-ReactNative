8736d7189e7afac3a69dbe02077d98ee93d6997e
Fredrik Faanes Slagsvold <fredrfsl@stud.ntnu.no>
2022-11-11 12:03:55 +0100

    feat: added navigation to dummyPage #7

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,17 +1,38 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import React from 'react';
 import HomePage from './page/Homepage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { RootStackParamList } from './types';
+import DummyPage from './page/DummyPage';
+
 
 const client = new ApolloClient({
   uri: 'http://it2810-20.idi.ntnu.no:3001/movie', 
   cache: new InMemoryCache(),
 });
 
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-        <HomePage/>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='HomePage'>
+
+          <Stack.Screen
+          name='HomePage'
+          component={HomePage}
+          />
+
+          <Stack.Screen
+          name='DummyPage'
+          component={DummyPage}
+          />
+
+          </Stack.Navigator>
+      </NavigationContainer>
     </ApolloProvider>
   );
 }
---
+++ frontend/types.ts
@@ -0,0 +1,4 @@
+export type RootStackParamList = {
+    HomePage: undefined
+    DummyPage: undefined
+}
