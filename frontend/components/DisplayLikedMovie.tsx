ac596269c43648395dba0b0324e6799bf8fd2a1c
Nicolai Nossum <nicolai.nossum@aboveit.no>
2022-11-13 18:18:10 +0100

    fixed import

--- frontend/components/DisplayLikedMovie.tsx
+++ frontend/components/DisplayLikedMovie.tsx
@@ -5,6 +5,7 @@ import { GET_MOVIEBYNAME } from "../utils/Queries";
 import { View, StyleSheet } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../types";
+import React from "react";
 
 
 type DisplayLikedMovieProps ={
